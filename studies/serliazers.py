d0c301287ca503eb44927b65b0599498999cace7
Artem Blodov <mr.samy@rambler.ru>
2024-03-30 16:19:20 +0200

    26.1

--- studies/serliazers.py
+++ studies/serliazers.py
@@ -16,6 +16,7 @@ class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lesson = LessonSerializer(many=True, read_only=True)
     usd_price = serializers.SerializerMethodField()
+    subscribe = serializers.SerializerMethodField()
 
     def get_lesson_count(self, obj):
         return obj.lesson.count()
@@ -23,10 +24,16 @@ class CourseSerializer(serializers.ModelSerializer):
     def get_usd_price(self, instance):
         return convert_price(instance.price)
 
+    def get_subscribe(self, obj):
+        user = self.context['request'].user.pk
+        return bool(Subscription.objects.filter(owner=user, course=obj.pk))
+
+
     class Meta:
         model = Course
         fields = ('name', 'lesson_count',
-                  'lesson', 'overview', 'picture', 'owner', 'usd_price')
+                  'lesson', 'overview', 'picture',
+                  'owner', 'usd_price', 'subscribe')
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
